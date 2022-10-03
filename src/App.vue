69bb292651c95f7034b2ce12948f8c4d4359c61f
Sylvester Nkeze <snkeze9@gmail.com>
2022-10-03 04:23:38 +0100

    Created interface and logic for todo app using Tailwind VueJs3 and Typescript

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,7 @@
-<script>
+<script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import Home from "./pages/Home.vue";
-export default {
-  components: {
-    Home,
-  },
-};
 </script>
 
 <template>
